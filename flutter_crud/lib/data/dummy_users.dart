a6b376eceb2103613255482d7339984553f42c8c
mnishimori <mnishimori@yahoo.com.br>
2020-08-11 12:43:57 -0300

    First commit

---
+++ flutter_crud/lib/data/dummy_users.dart
@@ -0,0 +1,28 @@
+import 'package:flutter_crud/models/user.dart';
+
+const DUMMY_USERS = {
+  '1': const User(
+      id: '1',
+      name: 'Maria',
+      email: 'maria@maria.com.br',
+      avatarUrl:
+          'https://pixabay.com/pt/vectors/avatar-feminino-menina-hijab-jovem-1606914/'),
+  '2': const User(
+      id: '2',
+      name: 'Rafael',
+      email: 'rafael@rafael.com.br',
+      avatarUrl:
+          'https://cdn.pixabay.com/photo/2013/07/13/10/07/man-156584__340.png'),
+  '3': const User(
+      id: '3',
+      name: 'Fernanda',
+      email: 'feranda@fernanda.com.br',
+      avatarUrl:
+          'https://pixabay.com/pt/vectors/usu%C3%A1rio-avatar-feminino-loiro-310807/'),
+  '4': const User(
+      id: '4',
+      name: 'Guilherme',
+      email: 'guilherme@guilherme.com.br',
+      avatarUrl:
+          'https://pixabay.com/pt/vectors/empres%C3%A1rio-masculino-neg%C3%B3cios-310819/')
+};
